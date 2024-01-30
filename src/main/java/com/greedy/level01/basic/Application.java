88299726d5ea7e32a79a7acc169a2f3ec2c450be
rlawlsdyd5932 <jyjy0509@naver.com>
2024-01-30 20:26:57 +0900

    commit

---
+++ src/main/java/com/greedy/level01/basic/Application.java
@@ -0,0 +1,20 @@
+package com.greedy.level01.basic;
+
+public class Application {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+
+        Calculator calc=new Calculator();
+        int sum=calc.sumTwoNumber(10,20);
+        System.out.println("sum = " + sum);
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/greedy/level01/basic/Calculator.java
@@ -0,0 +1,30 @@
+package com.greedy.level01.basic;
+
+public class Calculator {
+
+
+    public void checkMethod() {
+
+    }
+
+    public int sum1to10(int first,int last) {
+
+        return(sum1to10(1,10));
+
+    }
+
+
+    public static void checkMaxNumber(int a, int b) {
+        System.out.println((a>b)? a:b);
+
+
+    }
+
+    public int sumTwoNumber(int a, int b) {
+        return(a+b);
+    }
+
+    public int minusTwoNumber(int a, int b) {
+        return (a-b);
+    }
+}
